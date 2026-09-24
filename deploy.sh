102f1feced5cc244d6dd899aff31e4bcf324038c
Dusyaant R <dusyaant24@gmail.com>
2026-09-24 19:49:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+set -e
 
 echo "Tearing down old environment..."
-# The || true ensures the script doesn't fail if no container is currently running
 docker-compose down || true
 
 echo "Spinning up new container..."
-# -d runs it in detached mode (in the background)
 docker-compose up -d
 
-echo "Deployment complete! Application is running on Port 80."
+echo "Deployment complete!"
